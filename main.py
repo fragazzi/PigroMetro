353cf7a1f442dfaa1a7362ae3dbcad2192448f58
Francesco Ragazzi <68106916+fragazzi@users.noreply.github.com>
2023-07-19 10:39:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def main():
     st.markdown("Caricando delle registrazioni **audio** con le informazioni del viaggio fatto, l'app "
                 "automaticamente calcolerà il chilometraggio. Una registrazione deve avere la seguente struttura:")
     st.markdown("*data*   **'partenza'**   *luogo di partenza*   **'arrivo'**   *luogo di arrivo*")
-    st.markdown("Dove le parole **'partenza'** e **'arrivo'** devono essere presenti obbligatoriamente.")
+    st.markdown("Dove le parole **'partenza'** e **'arrivo'** devo essere presenti obbligatoriamente.")
     st.markdown("Un esempio di registrazione corretta è:")
     st.markdown("8 giugno 2023 **partenza**  Via Paolo Ferrari 85 Modena **arrivo** Via di San Luca 36 Bologna")
 
@@ -34,7 +34,7 @@ def main():
     uploaded_files = st.file_uploader("Seleziona file audio", type=["ogg", "mp3"], accept_multiple_files=True)
 
     # Data lists for table
-    id_list, file_list, date_list, dep_list, arr_list, km_list = [], [], [], [], [], []
+    id_list, date_list, dep_list, arr_list, km_list = [], [], [], [], []
 
     st.text(f"Modello usato: {model_selected}")
     with st.spinner('Calcolo delle distanze...'):
@@ -45,7 +45,6 @@ def main():
                 tokens = [t for t in tokens if t != '']
                 
                 id_list.append(file.id)
-                file_list.append(file.name)
             
                 date_tks = [tokens[0], tokens[1], tokens[2]]
                 date = transform_date(date_tks)
@@ -56,7 +55,7 @@ def main():
                 departure_tks = tokens[index_dep + 1:index_arr]
                 arrival_tks = tokens[index_arr + 1:]
 
-                departure = " ".join(departure_tks)
+                departure = " ".join(departure_tks[:-1])
                 arrival = " ".join(arrival_tks)
 
                 lat1, lon1 = geocode_place(departure)
@@ -66,14 +65,13 @@ def main():
                 if lat1 is not None and lon1 is not None and lat2 is not None and lon2 is not None:
                     distance = round(calculate_distance(lat1, lon1, lat2, lon2), 2)
 
-                date_list.append(date)
-                dep_list.append(departure)
-                arr_list.append(arrival)
+                date_list.append(date[:-1])
+                dep_list.append(departure[:-1])
+                arr_list.append(arrival[:-1])
                 km_list.append(distance)
 
     data_dict = {
         'ID': id_list,
-        'Nome file': file_list,
         'Data': date_list,
         'Partenza': dep_list,
         'Arrivo': arr_list,
@@ -84,14 +82,16 @@ def main():
     if not df.empty:
         st.data_editor(
             df,
-            disabled=["ID", "Nome file", "Data"],
+            disabled=["ID", "Data"],
             hide_index=True
         )
 
         if st.button("Download excel"):
             with st.spinner("Download in corso..."):
-                df.to_excel("Percorsi.xlsx", index=False)
+                try:
+                    df.to_excel("Percorsi.xlsx", index=False)
+                finally:
+                    st.text("Tabella scaricata!")
 
-
-if __name__ == "__main__":
-    main()
+#if __name__ == "__main__":
+#    main()
