b5b6527ba9bf9cbb5e47b02f7a406a8cd3b6a98a
Francesco Ragazzi <68106916+fragazzi@users.noreply.github.com>
2023-07-19 15:35:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from tempfile import NamedTemporaryFile
 from utils import *
 
 
-@st.cache
 def convert_df_to_csv(df):
  # IMPORTANT: Cache the conversion to prevent computation on every rerun
   return df.to_csv().encode('utf-8')
