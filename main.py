19481382160c6f75a981e4a50db91a37c1b0813e
Francesco Ragazzi <68106916+fragazzi@users.noreply.github.com>
2023-07-19 15:28:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ from utils import *
 
 @st.cache
 def convert_df_to_csv(df):
-  # IMPORTANT: Cache the conversion to prevent computation on every rerun
-  return df.to_excel() # .encode('utf-8')
+ # IMPORTANT: Cache the conversion to prevent computation on every rerun
+  return df.to_csv().encode('utf-8')
 
 
 st.title("PigroMetro    :flag-it:")
